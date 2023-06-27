a5a93383015d293ab12df7ea0d45af2fbf6b87aa
valentinnavalos <valentinnavalos@gmail.com>
2023-06-27 15:30:29 -0300

    refactor: group card

--- packages/web-app/src/components/groupCard/index.tsx
+++ packages/web-app/src/components/groupCard/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, {SyntheticEvent} from 'react';
+import React, { SyntheticEvent } from 'react';
 
 import FallbackImg from '../../../../ui-components/src/assets/avatar-token.svg'
 
@@ -19,32 +19,21 @@ export const CardGroup: React.FC<CardGroupProps> = ({
 
   return (
     <Card data-testid="cardToken" bgWhite={bgWhite}>
-      <CoinDetailsWithImage>
-        <CoinImage
+      <GroupDetailsWithImage>
+        <GroupImage
           src={props.groupImageUrl}
           onError={(e: SyntheticEvent<HTMLImageElement, Event>) => {
             e.currentTarget.src = FallbackImg;
           }}
         />
-        <CoinDetails>
-          <CoinNameAndAllocation>
-            <CoinName>{props.groupName}</CoinName>
-          </CoinNameAndAllocation>
-            <SecondaryCoinDetails>
-              <div className="flex space-x-0.5">
-                <div>{props.groupDescription}</div>
-              </div>
-            </SecondaryCoinDetails>
-        </CoinDetails>
-      </CoinDetailsWithImage>
+        <GroupDetails>
+          <GroupName>{props.groupName}</GroupName>
+          <SecondaryGroupDetails>{props.groupDescription}</SecondaryGroupDetails>
+        </GroupDetails>
+      </GroupDetailsWithImage>
       <MarketProperties>
-        <FiatValue>
-            <div className="truncate">{`${props.memberCount} Members`}</div>
-        </FiatValue>
-
-        <SecondaryFiatDetails>
-            <div className="truncate">{props.groupUSDValue}</div>
-        </SecondaryFiatDetails>
+        <MemberCount>{`${props.memberCount} Members`}</MemberCount>
+        <FiatValue>{props.groupUSDValue}</FiatValue>
       </MarketProperties>
     </Card>
   );
@@ -52,46 +41,40 @@ export const CardGroup: React.FC<CardGroupProps> = ({
 
 type CardProps = Pick<CardGroupProps, 'bgWhite'>;
 
-const Card = styled.div.attrs(({bgWhite}: CardProps) => ({
-  className: `flex justify-between space-x-4 items-center py-2.5 px-3 overflow-hidden ${
-    bgWhite ? 'bg-ui-50' : 'bg-ui-0'
-  } rounded-xl`,
-}))<CardProps>``;
+const Card = styled.div.attrs(({ bgWhite }: CardProps) => ({
+  className: `flex flex-col tablet:flex-row justify-between space-x-4 tablet:items-center py-1.5 tablet:py-2.5 px-2.5 tablet:px-3 overflow-hidden ${bgWhite ? 'bg-ui-50' : 'bg-ui-0'
+    } rounded-xl`,
+})) <CardProps>``;
 
-const CoinDetailsWithImage = styled.div.attrs({
+const GroupDetailsWithImage = styled.div.attrs({
   className: 'flex items-center flex-auto',
 })``;
 
-const CoinImage = styled.img.attrs(({src}) => ({
+const GroupImage = styled.img.attrs(({ src }) => ({
   className: 'w-3 h-3 tablet:h-5 tablet:w-5 rounded-full',
   src,
 }))``;
 
-const CoinDetails = styled.div.attrs({
-  className: 'ml-2 space-y-1 overflow-hidden',
+const GroupDetails = styled.div.attrs({
+  className: 'ml-2 tablet:space-y-1 overflow-hidden py-1 tablet:py-0',
 })``;
 
-const CoinNameAndAllocation = styled.div.attrs({
-  className: 'flex items-start space-x-1',
-})``;
-
-const CoinName = styled.h1.attrs({
+const GroupName = styled.h1.attrs({
   className: 'font-bold text-ui-800 truncate',
 })``;
 
-const SecondaryCoinDetails = styled.div.attrs({
+const SecondaryGroupDetails = styled.div.attrs({
   className: 'ft-text-sm text-ui-500 space-x-0.5 tablet:flex',
 })``;
 
 const MarketProperties = styled.div.attrs({
-  className: 'ft-text-sm text-right space-y-1 flex-auto overflow-hidden',
+  className: 'ft-text-sm ml-2 pl-1 tablet:pl-0 tablet:text-right tablet:space-y-1 tablet:flex-auto tablet:overflow-hidden',
 })``;
 
-const FiatValue = styled.h1.attrs({
-  className: 'text-ui-500',
+const MemberCount = styled.h1.attrs({
+  className: 'text-ui-500 my-[0.1rem] tablet:my-0',
 })``;
 
-const SecondaryFiatDetails = styled.div.attrs({
-  className:
-    'text-ui-500 space-x-1 flex justify-end items-center truncate',
+const FiatValue = styled.div.attrs({
+  className: 'text-ui-500 my-[0.1rem] tablet:my-0',
 })``;
