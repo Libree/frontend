b4a9cc42aedb31f8923837310b5b2bc332a2aee4
valentinnavalos <valentinnavalos@gmail.com>
2023-07-04 12:06:10 -0300

    refactor: credit delegation stepper

---
+++ packages/web-app/src/pages/newCreditDelegation.tsx
@@ -0,0 +1,73 @@
+import { withTransaction } from '@elastic/apm-rum-react';
+import React, { useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+import { Loading } from 'components/temporary';
+import { ActionsProvider } from 'context/actions';
+import { CreateProposalProvider } from 'context/createProposal';
+import { useDaoDetailsQuery } from 'hooks/useDaoDetails';
+import { PluginTypes } from 'hooks/usePluginClient';
+import { usePluginSettings } from 'hooks/usePluginSettings';
+import { InputValue } from '@aragon/ui-components';
+import CreditDelegationStepper from 'containers/creditDelegationStepper';
+
+export type CreditDelegationAction = {
+    to: InputValue;
+    from: string;
+    amount: string;
+    name: string; // This indicates the type of action; Deposit is NOT an action
+};
+
+type CreditDelegationFormData = {
+    actions: CreditDelegationAction[];
+
+    // Proposal data
+    address: string;
+};
+
+export const defaultValues = {
+    actions: [],
+};
+
+const NewCreditDelegation: React.FC = () => {
+    const [showTxModal, setShowTxModal] = useState(false);
+
+    const { data: daoDetails, isLoading: detailsLoading } = useDaoDetailsQuery();
+    const { data: pluginSettings, isLoading: settingsLoading } = usePluginSettings(
+        daoDetails?.plugins[0].instanceAddress as string,
+        daoDetails?.plugins[0].id as PluginTypes
+    );
+
+    const formMethods = useForm<CreditDelegationFormData>({
+        defaultValues,
+        mode: 'onChange',
+    });
+
+    /*************************************************
+     *                    Render                     *
+     *************************************************/
+
+    if (!daoDetails || !pluginSettings || detailsLoading || settingsLoading) {
+        return <Loading />;
+    }
+
+    return (
+        <>
+            <FormProvider {...formMethods}>
+                <ActionsProvider daoId={daoDetails?.address as string}>
+                    <CreateProposalProvider
+                        showTxModal={showTxModal}
+                        setShowTxModal={setShowTxModal}
+                    >
+                        <CreditDelegationStepper
+                            daoDetails={daoDetails}
+                            pluginSettings={pluginSettings}
+                            enableTxModal={() => setShowTxModal(true)}
+                        />
+                    </CreateProposalProvider>
+                </ActionsProvider>
+            </FormProvider>
+        </>
+    );
+};
+
+export default withTransaction('NewCreditDelegation', 'component')(NewCreditDelegation);
