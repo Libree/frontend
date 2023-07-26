da4f76b7dacc1f8f6425b01df3a5b0440a7cda4a
Ivan <ivan.alberquilla@gmail.com>
2023-07-26 17:39:44 +0200

    swap action

--- packages/web-app/src/utils/encoding.ts
+++ packages/web-app/src/utils/encoding.ts
@@ -6,7 +6,7 @@ import {
     SupportedNetworksArray,
     SupportedNetwork,
     DaoAction
-  } from '@aragon/sdk-client-common';
+} from '@aragon/sdk-client-common';
 
 import {
     hexToBytes,
@@ -16,6 +16,8 @@ import {
 import { CONTRACT_ADDRESSES, PLUGIN_ADDRESSES } from "./config";
 import { InterestRateType, PluginInstallItem } from "./types";
 import { CreditDelegator__factory } from "typechain-types/CreditDelegator__factory";
+import { ERC20__factory } from "typechain-types/ERC20__factory";
+import { Uniswapv3__factory } from "typechain-types/Uniswapv3__factory";
 import { Subgovernance__factory } from "typechain-types/Subgovernance__factory";
 import { VotingMode, VotingSettings } from "@aragon/sdk-client";
 
@@ -171,27 +173,81 @@ export const encodeActionsGroup = (
 
 export function votingSettingsToContract(
     params: VotingSettings,
-  ) {
+) {
     return [
-      BigNumber.from(
-        votingModeToContracts(params.votingMode || VotingMode.STANDARD),
-      ),
-      BigNumber.from(encodeRatio(params.supportThreshold, 6)),
-      BigNumber.from(encodeRatio(params.minParticipation, 6)),
-      BigNumber.from(params.minDuration),
-      BigNumber.from(params.minProposerVotingPower ?? 0),
+        BigNumber.from(
+            votingModeToContracts(params.votingMode || VotingMode.STANDARD),
+        ),
+        BigNumber.from(encodeRatio(params.supportThreshold, 6)),
+        BigNumber.from(encodeRatio(params.minParticipation, 6)),
+        BigNumber.from(params.minDuration),
+        BigNumber.from(params.minProposerVotingPower ?? 0),
     ];
-  }
+}
 
-  export function votingModeToContracts(votingMode: VotingMode): number {
-    switch (votingMode) {
-      case VotingMode.STANDARD:
-        return 0;
-      case VotingMode.EARLY_EXECUTION:
-        return 1;
-      case VotingMode.VOTE_REPLACEMENT:
-        return 2;
-      default:
-        throw new Error();
+
+export const encodeApproveAction = (
+    tokenAddress: string,
+    spender: string,
+    amount: string
+): DaoAction => {
+    const iface = ERC20__factory.createInterface()
+
+    const hexData = iface.encodeFunctionData(
+        'approve',
+        [spender, amount]
+    )
+
+    return {
+        to: tokenAddress,
+        value: ethers.utils.parseEther('0').toBigInt(),
+        data: hexToBytes(hexData)
     }
-  }
+}
+
+export const encodeSwapAction = (
+    tokenIn: string,
+    tokenOut: string,
+    fee: string,
+    recipient: string,
+    amountIn: string,
+    amountOutMinimum: string,
+    sqrtPriceLimitX96: string,
+    pluginAddress: string
+): DaoAction[] => {
+    const iface = Uniswapv3__factory.createInterface()
+
+    const hexData = iface.encodeFunctionData(
+        'swap',
+        [
+            tokenIn,
+            tokenOut,
+            fee,
+            recipient,
+            amountIn,
+            amountOutMinimum,
+            sqrtPriceLimitX96
+        ]
+    )
+
+    return [
+        { ...encodeApproveAction(tokenIn, pluginAddress, amountIn) },
+        {
+            to: pluginAddress,
+            value: ethers.utils.parseEther('0').toBigInt(),
+            data: hexToBytes(hexData)
+        }]
+}
+
+export function votingModeToContracts(votingMode: VotingMode): number {
+    switch (votingMode) {
+        case VotingMode.STANDARD:
+            return 0;
+        case VotingMode.EARLY_EXECUTION:
+            return 1;
+        case VotingMode.VOTE_REPLACEMENT:
+            return 2;
+        default:
+            throw new Error();
+    }
+}
